211b54c759b42ff01cd5c45330916566a5091391
Juan F. Esteban Müller <JuanF.EstebanMuller@esss.se>
2020-01-22 11:02:18 +0100

    Adding method to check if a LegendItem is selected

--- xaos.ui.plot.module/src/main/java/eu/ess/xaos/ui/plot/Legend.java
+++ xaos.ui.plot.module/src/main/java/eu/ess/xaos/ui/plot/Legend.java
@@ -16,7 +16,6 @@
  */
 package eu.ess.xaos.ui.plot;
 
-
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import javafx.beans.property.BooleanProperty;
@@ -37,252 +36,265 @@ import javafx.scene.layout.TilePane;
 import static javafx.geometry.Orientation.HORIZONTAL;
 import static javafx.geometry.Orientation.VERTICAL;
 
-
 /**
- * A chart legend that displays a list of items with symbols in a {@link TilePane}.
+ * A chart legend that displays a list of items with symbols in a
+ * {@link TilePane}.
  *
  * @author claudio.rosati@esss.se
  */
-@SuppressWarnings( "ClassWithoutLogger" )
+@SuppressWarnings("ClassWithoutLogger")
 public class Legend extends TilePane {
 
-	private static final int GAP = 6;
+    private static final int GAP = 6;
 
-	private ListChangeListener<LegendItem> itemsListener = c -> {
+    private ListChangeListener<LegendItem> itemsListener = c -> {
 
-		getChildren().setAll(getItems().stream().map(i -> i.checkBox).collect(Collectors.toList()));
+        getChildren().setAll(getItems().stream().map(i -> i.checkBox).collect(Collectors.toList()));
 
-		if ( isVisible() ) {
-			requestLayout();
-		}
+        if (isVisible()) {
+            requestLayout();
+        }
 
-	};
+    };
 
-	/*
-	 * *********************************************************************** *
-	 * START OF JAVAFX PROPERTIES *
-	 * ***********************************************************************
-	 */
+    /*
+     ************************************************************************
+     * START OF JAVAFX PROPERTIES                                           *
+     ************************************************************************
+     */
 
-	/*
-	 * ---- items --------------------------------------------------------------
-	 */
-	private final ObjectProperty<ObservableList<LegendItem>> items = new SimpleObjectProperty<>(this, "items") {
+    /*
+     * ---- items --------------------------------------------------------------
+     */
+    private final ObjectProperty<ObservableList<LegendItem>> items = new SimpleObjectProperty<>(this, "items") {
 
-		ObservableList<LegendItem> oldItems = null;
+        ObservableList<LegendItem> oldItems = null;
 
-		@Override protected void invalidated() {
+        @Override
+        protected void invalidated() {
 
-			if ( oldItems != null ) {
-				oldItems.removeListener(itemsListener);
-			}
+            if (oldItems != null) {
+                oldItems.removeListener(itemsListener);
+            }
 
-			getChildren().clear();
+            getChildren().clear();
 
-			ObservableList<LegendItem> newItems = get();
+            ObservableList<LegendItem> newItems = get();
 
-			if ( newItems != null ) {
-				newItems.addListener(itemsListener);
-				getChildren().addAll(newItems.stream().map(i -> i.checkBox).collect(Collectors.toList()));
-			}
+            if (newItems != null) {
+                newItems.addListener(itemsListener);
+                getChildren().addAll(newItems.stream().map(i -> i.checkBox).collect(Collectors.toList()));
+            }
 
-			oldItems = newItems;
+            oldItems = newItems;
 
-			requestLayout();
+            requestLayout();
 
-		}
+        }
 
-	};
-
-	/**
-	 * @return The legend items to display in this legend.
-	 */
-	public final ObjectProperty<ObservableList<LegendItem>> itemsProperty() {
-		return items;
-	}
-
-	public final void setItems( ObservableList<LegendItem> value ) {
-		itemsProperty().set(value);
-	}
-
-	public final ObservableList<LegendItem> getItems() {
-		return itemsProperty().get();
-	}
-
-	/*
-	 * ---- vertical -----------------------------------------------------------
-	 */
-	private final BooleanProperty vertical = new SimpleBooleanProperty(this, "vertical", false) {
-		@Override protected void invalidated() {
-			setOrientation(get() ? VERTICAL : HORIZONTAL);
-		}
-	};
+    };
 
     /**
-	 * @return Whether legend items should be laid out vertically in columns
-	 *         rather than horizontally in rows.
-	 */
-	public final BooleanProperty verticalProperty() {
-		return vertical;
-	}
+     * @return The legend items to display in this legend.
+     */
+    public final ObjectProperty<ObservableList<LegendItem>> itemsProperty() {
+        return items;
+    }
 
-	public final boolean isVertical() {
-		return verticalProperty().get();
-	}
+    public final void setItems(ObservableList<LegendItem> value) {
+        itemsProperty().set(value);
+    }
 
-	public final void setVertical( boolean value ) {
-		verticalProperty().set(value);
-	}
+    public final ObservableList<LegendItem> getItems() {
+        return itemsProperty().get();
+    }
 
-	/*
-	 * *********************************************************************** *
-	 * END OF JAVAFX PROPERTIES *
-	 * ***********************************************************************
-	 */
+    /*
+     * ---- vertical -----------------------------------------------------------
+     */
+    private final BooleanProperty vertical = new SimpleBooleanProperty(this, "vertical", false) {
+        @Override
+        protected void invalidated() {
+            setOrientation(get() ? VERTICAL : HORIZONTAL);
+        }
+    };
 
-	public Legend() {
+    /**
+     * @return Whether legend items should be laid out vertically in columns
+     * rather than horizontally in rows.
+     */
+    public final BooleanProperty verticalProperty() {
+        return vertical;
+    }
 
-		super(GAP, GAP);
+    public final boolean isVertical() {
+        return verticalProperty().get();
+    }
 
-		getStyleClass().setAll("chart-legend");
-		setId("legend");
-		setTileAlignment(Pos.CENTER_LEFT);
-		setItems(FXCollections.observableArrayList());
+    public final void setVertical(boolean value) {
+        verticalProperty().set(value);
+    }
 
-	}
+    /*
+     ************************************************************************
+     * END OF JAVAFX PROPERTIES                                             *
+     ************************************************************************
+     */
+    public Legend() {
 
-	@Override
-	protected double computePrefHeight( double forWidth ) {
-		//	Legend prefHeight is zero if there are no legend items.
-		return ( getItems().size() > 0 ) ? super.computePrefHeight(forWidth) : 0;
-	}
+        super(GAP, GAP);
 
-	@Override
-	protected double computePrefWidth( double forHeight ) {
-		//	Legend prefWidth is zero if there are no legend items.
-		return ( getItems().size() > 0 ) ? super.computePrefWidth(forHeight) : 0;
-	}
+        getStyleClass().setAll("chart-legend");
+        setId("legend");
+        setTileAlignment(Pos.CENTER_LEFT);
+        setItems(FXCollections.observableArrayList());
 
-	/**
-	 * A item to be displayed on a Legend.
-	 * <p>
-	 * It is realized by a {@link CheckBox} whose text is the series name, and
-	 * its color is the series one.</p>
-	 *
-	 * @css.class {@code chart-legend-item} for the legend text.
-	 * @css.class {@code chart-legend-item-symbol} It should be defined if a
-	 *            special default symbol is wanted. If defined it will take
-	 *            precedence of the others.
-	 * @css.class {@code chart-symbol}, {@code default-color1.chart-symbol},
-	 *            {@code default-color2.chart-symbol}, {@code default-color3.chart-symbol},
-	 *            {@code default-color4.chart-symbol}, {@code default-color5.chart-symbol},
-	 *            {@code default-color6.chart-symbol} and {@code default-color6.chart-symbol}
-	 *            for scatter charts.
-	 * @css.class {@code chart-line-symbol} for line charts.
-	 * @css.class {@code chart-area-symbol} and {@code area-legend-symbol} for
-	 *            area charts.
-	 * @css.class {@code bubble-legend-symbol} for bubble charts.
-	 * @css.class {@code bar-legend-symbol} for bar charts.
-	 */
-	@SuppressWarnings( "PublicInnerClass" )
-	public static class LegendItem {
+    }
 
-		/**
-		 * CheckBox used to represent the legend item.
-		 */
-		@SuppressWarnings( "PackageVisibleField" )
-		final CheckBox checkBox = new CheckBox();
+    @Override
+    protected double computePrefHeight(double forWidth) {
+        //	Legend prefHeight is zero if there are no legend items.
+        return (getItems().size() > 0) ? super.computePrefHeight(forWidth) : 0;
+    }
 
-		/*
-		 * ******************************************************************* *
-		 * START OF JAVAFX PROPERTIES *
-		 * *******************************************************************
-		 */
+    @Override
+    protected double computePrefWidth(double forHeight) {
+        //	Legend prefWidth is zero if there are no legend items.
+        return (getItems().size() > 0) ? super.computePrefWidth(forHeight) : 0;
+    }
 
-		/*
-		 * ---- symbol ---------------------------------------------------------
-		 */
-		private final ObjectProperty<Node> symbol = new SimpleObjectProperty<>(LegendItem.this, "symbol", new Region()) {
-			@Override
-			protected void invalidated() {
+    /**
+     * A item to be displayed on a Legend.
+     * <p>
+     * It is realized by a {@link CheckBox} whose text is the series name, and
+     * its color is the series one.</p>
+     *
+     * @css.class {@code chart-legend-item} for the legend text.
+     * @css.class {@code chart-legend-item-symbol} It should be defined if a
+     * special default symbol is wanted. If defined it will take precedence of
+     * the others.
+     * @css.class {@code chart-symbol}, {@code default-color1.chart-symbol},
+     * {@code default-color2.chart-symbol}, {@code default-color3.chart-symbol},
+     * {@code default-color4.chart-symbol}, {@code default-color5.chart-symbol},
+     * {@code default-color6.chart-symbol} and
+     * {@code default-color6.chart-symbol} for scatter charts.
+     * @css.class {@code chart-line-symbol} for line charts.
+     * @css.class {@code chart-area-symbol} and {@code area-legend-symbol} for
+     * area charts.
+     * @css.class {@code bubble-legend-symbol} for bubble charts.
+     * @css.class {@code bar-legend-symbol} for bar charts.
+     */
+    @SuppressWarnings("PublicInnerClass")
+    public static class LegendItem {
 
-				Node symbol = get();
+        /**
+         * CheckBox used to represent the legend item.
+         */
+        @SuppressWarnings("PackageVisibleField")
+        final CheckBox checkBox = new CheckBox();
 
-				if ( symbol != null ) {
-					symbol.getStyleClass().setAll("chart-legend-item-symbol");
-				}
+        /*
+         *********************************************************************
+	* START OF JAVAFX PROPERTIES                                        *
+	* *******************************************************************
+         */
 
-				checkBox.setGraphic(symbol);
+         /*
+	 * ---- symbol ---------------------------------------------------------
+         */
+        private final ObjectProperty<Node> symbol = new SimpleObjectProperty<>(LegendItem.this, "symbol", new Region()) {
+            @Override
+            protected void invalidated() {
 
-			}
-		};
+                Node symbol = get();
 
-		/**
-		 * @return The symbol to use next to the item text, set to {@code null}
-		 *         for no symbol. The default is a simple circle.
-		 */
-		public final ObjectProperty<Node> symbolProperty() {
-			return symbol;
-		}
+                if (symbol != null) {
+                    symbol.getStyleClass().setAll("chart-legend-item-symbol");
+                }
 
-		public final Node getSymbol() {
-			return symbol.getValue();
-		}
+                checkBox.setGraphic(symbol);
 
-		public final void setSymbol( Node value ) {
-			symbol.setValue(value);
-		}
+            }
+        };
 
-		/*
-		 * ---- text -----------------------------------------------------------
-		 */
-		public final StringProperty textProperty() {
-			return checkBox.textProperty();
-		}
+        /**
+         * @return The symbol to use next to the item text, set to {@code null}
+         * for no symbol. The default is a simple circle.
+         */
+        public final ObjectProperty<Node> symbolProperty() {
+            return symbol;
+        }
 
-		public final String getText() {
-			return checkBox.getText();
-		}
+        public final Node getSymbol() {
+            return symbol.getValue();
+        }
 
-		public final void setText( String value ) {
-			checkBox.setText(value);
-		}
+        public final void setSymbol(Node value) {
+            symbol.setValue(value);
+        }
 
-		/*
-		 * ******************************************************************* *
-		 * END OF JAVAFX PROPERTIES *
-		 * *******************************************************************
-		 */
+        /*
+         * ---- text -----------------------------------------------------------
+         */
+        public final StringProperty textProperty() {
+            return checkBox.textProperty();
+        }
 
-		public LegendItem( String text, Consumer<Boolean> checkBoxSelectionHandler ) {
+        public final String getText() {
+            return checkBox.getText();
+        }
 
-			checkBox.getStyleClass().add("chart-legend-item");
-			checkBox.setAlignment(Pos.CENTER_LEFT);
-			checkBox.setContentDisplay(ContentDisplay.LEFT);
-			checkBox.setGraphic(getSymbol());
-			checkBox.setId("legend-check-box: " + text);
-			checkBox.setSelected(true);
-			checkBox.selectedProperty().addListener(( ob, ov, nv ) -> {
-				if ( checkBoxSelectionHandler != null ) {
-					checkBoxSelectionHandler.accept(nv);
-				}
-			});
+        public final void setText(String value) {
+            checkBox.setText(value);
+        }
 
-			getSymbol().getStyleClass().setAll("chart-legend-item-symbol");
-			setText(text);
 
-		}
+        /*
+         * ---- status -----------------------------------------------------------
+         */
+        public final boolean isSelected() {
+            return checkBox.isSelected();
+        }
 
-		public LegendItem( String text, Node symbol, Consumer<Boolean> checkBoxSelectionHandler ) {
-			this(text, checkBoxSelectionHandler);
-			setSymbol(symbol);
-		}
+        public final void setSelected(boolean selected) {
+            checkBox.setSelected(selected);
+        }
 
-		@Override
-		public String toString() {
-			return getText();
-		}
+        /*
+	* *******************************************************************
+	* END OF JAVAFX PROPERTIES                                          *
+	* *******************************************************************
+         */
 
-	}
+        public LegendItem(String text, Consumer<Boolean> checkBoxSelectionHandler) {
+
+            checkBox.getStyleClass().add("chart-legend-item");
+            checkBox.setAlignment(Pos.CENTER_LEFT);
+            checkBox.setContentDisplay(ContentDisplay.LEFT);
+            checkBox.setGraphic(getSymbol());
+            checkBox.setId("legend-check-box: " + text);
+            checkBox.setSelected(true);
+            checkBox.selectedProperty().addListener((ob, ov, nv) -> {
+                if (checkBoxSelectionHandler != null) {
+                    checkBoxSelectionHandler.accept(nv);
+                }
+            });
+
+            getSymbol().getStyleClass().setAll("chart-legend-item-symbol");
+            setText(text);
+
+        }
+
+        public LegendItem(String text, Node symbol, Consumer<Boolean> checkBoxSelectionHandler) {
+            this(text, checkBoxSelectionHandler);
+            setSymbol(symbol);
+        }
+
+        @Override
+        public String toString() {
+            return getText();
+        }
+
+    }
 
 }
